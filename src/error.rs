6a83e650ce0372e928864633afba335f6e00696c
aujxn <ajn6@pdx.edu>
2019-10-30 19:35:54 -0700

    new license - MPL2

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 /// This is the general error type for our library. It can represent any failed
 /// operation on a matrix, including building a new matrix or a failed operation
 /// like matrix multiplication
